acc1a9df242faaef302d5054ae5421bcd005fc03
Adrian ViRa <adrianvira@example.com>
2026-03-22 15:49:15 -0600

    Fix: Incluir apiCalls en la consulta de Prisma para Superadmin

--- app/api/superadmin/teachers/route.ts
+++ app/api/superadmin/teachers/route.ts
@@ -26,7 +26,8 @@ export async function GET(req: Request) {
                                     where: { role: "STUDENT" }
                                 },
                             }
-                        }
+                        },
+                        apiCalls: true
                     }
                 }
             },
